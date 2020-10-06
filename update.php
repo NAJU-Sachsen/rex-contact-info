0162478847100ee8f83179f9b495583aee15ace3
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-06 23:23:03 +0200

    Fix version compare in update routine

--- update.php
+++ update.php
@@ -2,7 +2,7 @@
 
 $t_contact_info = 'naju_contact_info';
 
-if (rex_version::compare($this->getVersion(), '0.0.2', '>=')) {
+if (rex_version::compare($this->getVersion(), '0.0.2', '<')) {
 	// Update from 0.0.1 to something newer
 	rex_sql_table::get(rex::getTable($t_contact_info))
 		->ensureColumn(new rex_sql_column('instagram', 'varchar(120)', false, ''))
@@ -10,7 +10,7 @@ if (rex_version::compare($this->getVersion(), '0.0.2', '>=')) {
 		->alter();
 }
 
-if (rex_version::compare($this->getVersion(), '0.0.3', '>=')) {
+if (rex_version::compare($this->getVersion(), '0.0.3', '<')) {
 	// Update from 0.0.2 to something newer
 	rex_sql_table::get(rex::getTable($t_contact_info))
 		->ensureColumn(new rex_sql_column('business_hours', 'varchar(255)', $default = ''))
