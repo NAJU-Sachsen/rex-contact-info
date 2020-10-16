182b9e44116203bfbbdb0f326d58ea81426ff690
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-10-16 18:54:56 +0200

    #2 - Fix reference to contact info table
    Resolves #2

--- update.php
+++ update.php
@@ -4,7 +4,7 @@ $t_contact_info = 'naju_contact_info';
 
 if (rex_version::compare($this->getVersion(), '0.0.2', '<')) {
 	// Update from 0.0.1 to something newer
-	rex_sql_table::get(rex::getTable($t_contact_info))
+	rex_sql_table::get($t_contact_info)
 		->ensureColumn(new rex_sql_column('instagram', 'varchar(120)', false, ''))
 		->ensureColumn(new rex_sql_column('facebook', 'varchar(120)', false, ''))
 		->alter();
@@ -12,7 +12,7 @@ if (rex_version::compare($this->getVersion(), '0.0.2', '<')) {
 
 if (rex_version::compare($this->getVersion(), '0.0.3', '<')) {
 	// Update from 0.0.2 to something newer
-	rex_sql_table::get(rex::getTable($t_contact_info))
+	rex_sql_table::get($t_contact_info)
 		->ensureColumn(new rex_sql_column('business_hours', 'varchar(255)', $default = ''))
 		->alter();
 }
